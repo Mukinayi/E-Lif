6347ff916f96dffe63582483c14af21d368dbac4
EXACT-IT-DEV <mukinayiseth@outlook.com>
2018-04-04 17:09:57 +0100

    recent recent recent

--- app/src/main/java/com/wolfsoft/one/cg/splashscreen/NoImei.java
+++ app/src/main/java/com/wolfsoft/one/cg/splashscreen/NoImei.java
@@ -1,7 +1,9 @@
 package com.wolfsoft.one.cg.splashscreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.wolfsoft.one.cg.R;
 
@@ -11,5 +13,9 @@ public class NoImei extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_imei);
+
+        TextView tv = (TextView)findViewById(R.id.tvMess);
+        Intent intent = getIntent();
+        tv.setText(intent.getStringExtra("message"));
     }
 }
